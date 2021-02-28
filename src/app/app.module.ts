feb27a139f8c63644e087430b87cc3588642e589
703Pvrtcx <kasaokapapkies@gmail.com>
2021-02-28 18:19:36 +0200

    Created Angular Project, Added Angular Material Module....Blank Project

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AngularMaterialModule } from "../app/components/sharedComponents/angular-material.module";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+ 
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AngularMaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
